71d392a639d4fc826508420008596af566d809a8
somiahgit <somiahjamal1997@gmail.com>
2020-10-10 14:44:33 +0300

    Initial commit

---
+++ app/src/main/java/somiah/jad/criminalintent/CrimeListFragment.kt
@@ -0,0 +1,128 @@
+package somiah.jad.criminalintent
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.res.ColorStateListInflaterCompat.inflate
+import androidx.core.content.res.ComplexColorCompat.inflate
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import java.text.DateFormat
+import java.util.zip.DataFormatException
+
+//private const val TAG = "CrimeListFragment"
+class CrimeListFragment : Fragment() {
+
+    private lateinit var crimeRecyclerView: RecyclerView
+    private var adapter:CrimeAdapter? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_crime_list, container, false)
+
+        crimeRecyclerView= view.findViewById(R.id.crime_recycler_view)as RecyclerView
+        crimeRecyclerView.layoutManager=LinearLayoutManager(context)
+
+        updateUI()
+
+        return view
+    }
+    private fun updateUI(){
+        val crimes = crimeListViewModel.crimes
+        adapter = CrimeAdapter(crimes)
+        crimeRecyclerView.adapter = adapter
+    }
+
+    private inner class CrimeHolder(view: View): RecyclerView.ViewHolder(view), View.OnClickListener {
+
+        private lateinit var crime : Crime
+        val titleTextView: TextView = itemView.findViewById(R.id.crime_title)
+        val dateTextView: TextView = itemView.findViewById(R.id.crime_date)
+        private val solvedImageView: ImageView = itemView.findViewById(R.id.crime_solved)
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+        fun bind(crime: Crime){
+            this.crime=crime
+            titleTextView.text = this.crime.title
+            dateTextView.text = DateFormat.getDateInstance(DateFormat.FULL).format(this.crime.date).toString()
+            solvedImageView.visibility = if (crime.isSolved){
+                View.VISIBLE
+            }else{
+                View.GONE
+            }
+
+
+        }
+
+
+
+        override fun onClick(v: View) {
+            Toast.makeText(context, "${crime.title} pressed!",
+                Toast.LENGTH_SHORT)
+                .show()
+
+        }
+    }
+
+    private inner class CrimeAdapter(var crimes: List<Crime>): RecyclerView.Adapter<CrimeHolder>(){
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CrimeHolder {
+
+            return if (viewType == R.layout.item_solution){
+                val view = layoutInflater.inflate(R.layout.item_solution,parent,false)
+                CrimeHolder(view)
+            }else{
+                val view = layoutInflater.inflate(R.layout.item_view,parent,false)
+                CrimeHolder(view)
+            }
+        }
+
+        override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
+
+            val crime = crimes[position]
+//            holder.apply {
+//                titleTextView.text= crime.title
+//                dateTextView.text = crime.date.toString()
+//            }
+
+            holder.bind(crime)
+        }
+
+        override fun getItemCount()= crimes.size
+        override fun getItemViewType(position: Int): Int {
+            return if(crimes[position].police){
+                R.layout.item_solution
+            }else
+            {
+                R.layout.item_view
+            }
+        }
+    }
+
+    private val crimeListViewModel: CrimeListViewModel by lazy {
+        ViewModelProviders.of(this).get(CrimeListViewModel::class.java)
+    }
+
+    companion object{
+        fun newInstance():CrimeListFragment{
+            return CrimeListFragment()
+        }
+    }
+}
